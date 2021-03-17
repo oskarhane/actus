996037ca873bcd83fe2df2bf1aa9903d6743fd11
Oskar Hane <oh@oskarhane.com>
2021-03-17 18:17:54 +0100

    Fix parser param bug + add tests

--- src/grammar/parse-input.ne
+++ src/grammar/parse-input.ne
@@ -1,5 +1,4 @@
 @preprocessor typescript
-
 main -> cmd params _ {% d => [d[0], d[1]]%}
 	  | cmd _ {% d => [d[0]] %}
 
@@ -7,10 +6,10 @@ cmd -> cmdToken {% d => d.join('').trim() %}
 
 params -> param:+ {% d => ({...d[0].reduce((all, dd) => ({...all, ...dd}), {})}) %}
 
-param -> paramName {% d => ({[d[0]]: null}) %}
+param -> paramName {% d => d[0] !== null ? {[d[0]]: null} : {} %}
 	   | paramName __ paramVal {% d => ({[d[0]]: d[2]}) %}
 
-paramName -> __ FLAG_START [^- ]:+ {% d => d[2].join('').trim() %}
+paramName -> __ FLAG_START [^- ]:* {% d => d[2].join('').trim() || null %}
 
 paramVal -> paramValToken {% id %}
 
---
+++ src/grammar/parser.test.ts
@@ -0,0 +1,53 @@
+import nearley from "nearley";
+import type { ParserResult } from "../types";
+import grammar from "./input-parser";
+
+const newParser = () => new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+
+test("should parse commands only correctly", () => {
+    let input = "t";
+    let parser = newParser();
+    parser.feed(input);
+    let parserResult: ParserResult = parser.results[0] as ParserResult;
+    expect(parserResult).toEqual(["t"]);
+
+    input = "ttt";
+    parser = newParser();
+    parser.feed(input);
+    parserResult = parser.results[0] as ParserResult;
+    expect(parserResult).toEqual(["ttt"]);
+
+    input = "ttt ttt";
+    parser = newParser();
+    parser.feed(input);
+    parserResult = parser.results[0] as ParserResult;
+    expect(parserResult).toEqual(["ttt ttt"]);
+
+    input = "ttt ttt   ttt";
+    parser = newParser();
+    parser.feed(input);
+    parserResult = parser.results[0] as ParserResult;
+    expect(parserResult).toEqual(["ttt ttt   ttt"]);
+});
+
+test("should parse commands with params correctly", () => {
+    const input = "tt -r -h -p 8080";
+    const parser = newParser();
+    parser.feed(input);
+    const parserResult: ParserResult = parser.results[0] as ParserResult;
+    expect(parserResult).toEqual(["tt", { r: null, h: null, p: "8080" }]);
+});
+
+test("should parse commands with empty params correctly", () => {
+    let input = "tt -r -h -p 8080 -";
+    let parser = newParser();
+    parser.feed(input);
+    let parserResult: ParserResult = parser.results[0] as ParserResult;
+    expect(parserResult).toEqual(["tt", { r: null, h: null, p: "8080" }]);
+
+    input = "tt -";
+    parser = newParser();
+    parser.feed(input);
+    parserResult = parser.results[0] as ParserResult;
+    expect(parserResult).toEqual(["tt", {}]);
+});
